3ca5235764274d3c268f75b78199e0966bda7519
Horizon008 <116902866+Horizon008@users.noreply.github.com>
2023-12-26 19:25:13 +0300

    Add files via upload

---
+++ Python for Data Science /Библиотека numpy/Зад1.py
@@ -0,0 +1,7 @@
+﻿import numpy as np
+data = np.genfromtxt(r'C:\Users\Egor\Desktop\Питон\Задания\Python\Python\Часть2\Библиотека numpy\mat.txt', delimiter=',')
+identity_matrix = np.genfromtxt(r'C:\Users\Egor\Desktop\Питон\Задания\Python\Python\Часть2\Библиотека numpy\ed.txt', delimiter=',')
+
+result = np.dot(data, identity_matrix)
+print (result)
+np.savetxt(r'C:\Users\Egor\Desktop\Питон\Задания\Python\Python\Часть2\Библиотека numpy\output.txt', result, fmt='%d', delimiter=',') 
